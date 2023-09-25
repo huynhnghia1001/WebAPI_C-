7d0dc230f32e6a36f2d3ca4bc4279dcef2a9813c
huynhnghia1001 <huynhnghia687@gmail.com>
2023-09-25 09:10:51 +0700

    update

---
+++ WEBAPI_C#/Controllers/HangHoaController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WEBAPI_C_.Models;
+
+namespace WEBAPI_C_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hangHoas);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                //LINQ [Query] Object
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                return Ok(hangHoa);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteID(string id)
+        {
+            try
+            {
+                //LINQ [Query] Object
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                hangHoas.Remove(hangHoa);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+        }
+        [HttpPost]
+        public IActionResult Create(HangHoa hangHoaVM)
+        {
+            var hanghoa = new HangHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = hangHoaVM.TenHangHoa,
+                DonGia = hangHoaVM.DonGia
+            };
+            hangHoas.Add(hanghoa);
+            return Ok( new
+            {
+                Success = true , Data = hanghoa
+            });
+        }
+    }
+}
---
+++ WEBAPI_C#/Models/HangHoa.cs
@@ -0,0 +1,12 @@
+﻿namespace WEBAPI_C_.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+        public double DonGia { get; set; }
+    }
+    public class HangHoa : HangHoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+    }
+}
